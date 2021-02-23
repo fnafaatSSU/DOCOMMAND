247204e1de13bae19e45e8284eb263b97007bdcc
fnafakent <fnafakentfnafa@kent.edu>
2021-02-22 20:37:09 -0500

    This is a command line

---
+++ Lab02.java
@@ -0,0 +1,25 @@
+class Lab02
+{ 
+/*
+   //Please print your name as a test for this file
+    public static void main(String args[]) 
+    { 
+        //1.	Fill 2D array with values from the user.
+        //2.	Write method sum which accepts a 2D array of integers and returns the sum of all of the elements. Use row-column traversal method.
+                rowSum: rowSum accepts two parameters: a 2D array of integers and an integer row. rowSum returns the sum of the integers of elements in the row given by row. 
+                colSum: colSum accepts two parameters: a 2D array of integers and an integer col. colSum returns the sum of the integers of elements in the column given by col. 
+                sum2: This method is the same as sum above, but you must use rowSum method in your code. 
+
+       // 3.	printTranspose: Given 2D array of integers, print the transpose of the array. The transpose of a 2D array is the array whose rows are the columns of the original array. 
+                Do not create a new array, instead, use for loops to traverse the original array. 
+                  If mat={{1,2,3},
+                          {4,5,6}}; 
+                   printTranspose(mat) 
+                      will print: 
+                   1 4
+                   2 5
+                   3 6
+*/
+
+    } 
+} 
